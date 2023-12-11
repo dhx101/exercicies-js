7457dcfa89ba55939cad5701b1481bd6960cfc2a
dhx101 <138037766+dhx101@users.noreply.github.com>
2023-12-11 15:12:33 +0100

    finished all exercises in S1

---
+++ S1/5.loops/exercise-3.js
@@ -0,0 +1,7 @@
+for (i = 0; i <= 10; i++) {
+	if (i < 10) {
+		console.log("Intentando Dormir");
+	} else {
+		console.log("Dormido!");
+	}
+}
---
+++ S1/extra/exercise-1.js
@@ -0,0 +1,1 @@
+const users = [{name: "Abel", years: 43}, {name: "Maria", years: 18}, {name: "Pedro", years: 14}, {name: "Samantha", years: 32}, {name: "Raquel", years: 16}];
