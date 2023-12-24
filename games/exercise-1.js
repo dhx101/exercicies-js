ca5c88c21e75fd4988bba877f364d2b48235ff42
David <138037766+dhx101@users.noreply.github.com>
2023-12-24 13:59:25 +0100

    made game 1 and 2

--- games/exercise-1.js
+++ games/exercise-1.js
@@ -64,42 +64,44 @@ let cardArray = [
 const myGrid = document.querySelector('[data-function="grid"]');
 const myScore = document.querySelector('[data-function="score"]');
 const myAttepts = document.querySelector('[data-function="attempts"]');
-// console.log(myScore, myAttepts);
 const valuesArray = [];
 let countScore = 0;
 let countAttemps = 0;
 cardArray.forEach((item) => {
-	const myCard$$ = document.createElement("img");
-	myCard$$.setAttribute("src", item.img);
-	myCard$$.setAttribute("alt", item.name);
+	const myCard$$ = document.createElement("div");
+	const myImg$$ = document.createElement("img");
+	myCard$$.appendChild(myImg$$);
 	myGrid.appendChild(myCard$$);
+	myImg$$.setAttribute("src", item.img);
+	myImg$$.setAttribute("alt", item.name);
+
+	myCard$$.addEventListener("click", () => {
+		addValueToArray(myImg$$, myCard$$);
+	});
 });
 
 const addValueToArray = (item) => {
-    
-    console.log("click")
-	// console.log(item.target.classList);
-	if (item.target.alt != undefined && item.target.classList != "selected" && item.target.src != "public/exercise-1/tick.svg") {
-		valuesArray.unshift(item.target.alt);
-        console.log(valuesArray)
+	console.log(item.classList == "selected");
+	if (item.classList != "selected" && !item.src.includes("tick.svg")) {
+		item.classList.add("selected");
+		valuesArray.unshift(item);
 	}
 	if (valuesArray.length >= 2) {
 		myAttepts.textContent = ++countAttemps;
-		if (valuesArray[1] == valuesArray[0]) {
-			myScore.textContent = ++countScore;
-			item.target.src = "public/exercise-1/tick.svg"
-		}
-		valuesArray.splice(0, 2);
+		setTimeout(
+			() => {
+				if (valuesArray[0].src == valuesArray[1].src) {
+					myScore.textContent = ++countScore;
+					valuesArray[0].src = "public/exercise-1/tick.svg";
+					valuesArray[1].src = "public/exercise-1/tick.svg";
+				} else {
+					valuesArray[0].classList.remove("selected");
+					valuesArray[1].classList.remove("selected");
+				}
+				valuesArray.splice(0, 2);
+			},
+			500,
+			valuesArray
+		);
 	}
-	// console.log(valuesArray)
 };
-
-
-
-myGrid.addEventListener("click", (event) => {
-	let item = event.target;
-	// console.log(item);
-	
-	addValueToArray(event);
-    item.classList.add("selected");
-});
--- games/exercise-2.js
+++ games/exercise-2.js
@@ -1,3 +1,76 @@
+const square = document.querySelectorAll('[data-function="square"]');
+const time = document.querySelector('[data-function="time-left"]');
+const score = document.querySelector('[data-function="score"]');
+const newModal = document.querySelector('.modal-2')
+const getNewSquare = (dificulty) => {
+	const randomNumber = Math.floor(Math.random() * square.length);
+	const randomSquare = square[randomNumber];
+	randomSquare.classList.toggle("b-mole");
+	randomSquare.addEventListener(
+		"click",
+		(myEvent = () => {
+			ganarPunto();
+			randomSquare.removeEventListener("click", myEvent);
+		})
+	);
+	console.log(myEvent);
+	setTimeout(() => {
+		randomSquare.classList.toggle("b-mole");
+		randomSquare.removeEventListener("click", myEvent);
+	}, dificulty);
+};
 
+const ganarPunto = () => {
+	++score.textContent;
+};
+const timeDown = () => {
+	--time.textContent;
+};
 
+const startBtn = document.createElement("button");
+startBtn.textContent = "Start game";
+const startGameDiv = document.querySelector(".start-game");
+startGameDiv.appendChild(startBtn);
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+close.addEventListener("click", () => {
+	modal.classList.remove("visible");
+});
+startBtn.addEventListener("click", () => {
+	modal.classList.add("visible");
+});
 
+const easy = document.querySelector(".easy");
+const medium = document.querySelector(".medium");
+const hard = document.querySelector(".hard");
+
+const newGame = (dificulty) => {
+	const game = setInterval(getNewSquare, 1000, dificulty);
+	const timer = setInterval(timeDown, 1000);
+	setTimeout(() => {
+		clearInterval(game);
+		clearInterval(timer);
+
+		const myP$$ = document.querySelector('.your-score')
+		myP$$.textContent = `Your score is ${score.textContent}`
+		newModal.classList.add('visible')
+	}, 30000);
+};
+const close2 = document.querySelector('.close-2')
+close2.addEventListener('click', ()=> {
+	newModal.classList.remove('visible')
+})
+easy.addEventListener("click", () => {
+	newGame(700);
+	modal.classList.remove("visible");
+});
+
+medium.addEventListener("click", () => {
+	newGame(500);
+	modal.classList.remove("visible");
+});
+
+hard.addEventListener("click", () => {
+	newGame(300);
+	modal.classList.remove("visible");
+});
